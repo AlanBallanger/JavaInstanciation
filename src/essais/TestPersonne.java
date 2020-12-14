ac1ad3dc12bf55c8c80ea4101997630550b742ba
DESKTOP-5UGPIRK\PC <alan.ballanger@live.fr>
2020-12-14 11:09:57 +0100

    final commit

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -8,7 +8,7 @@ public class TestPersonne {
 
         pers1.personnee("alan", "alan", "alan rue alan");
         System.out.println(pers1.printName("ballanger", "alan"));
-        pers1.modifyName("la babouche");
+        pers1.modifyName("la  babouche");
         pers1.modifySubname("alouch");
     }
 }
