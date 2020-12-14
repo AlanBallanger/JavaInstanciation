064c5e4a08464c066de20708fd4d612f3cccd8de
DESKTOP-5UGPIRK\PC <alan.ballanger@live.fr>
2020-12-14 11:08:46 +0100

    first commit

--- src/entities/Personne.java
+++ src/entities/Personne.java
@@ -6,9 +6,36 @@ public class Personne {
     String prenom;
     String adresse;
 
-    public Personne (String nom, String prenom, String addresse) {
+    public Personne (String nom, String prenom, String adresse) {
         this.nom = nom;
         this.prenom = prenom;
         this.adresse = adresse;
+    }
+    public void personnee (String nom, String prenom, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+    }
+    public String printName (String nom, String prenom) {
+        return "Bonjour monsieur " + nom.toUpperCase() + " " + prenom;
+    }
+
+    public void modifyName (String nom) {
+        this.nom = nom;
+    }
+    public void modifySubname (String prenom) {
+        this.prenom = prenom;
+    }
+    public void modifyAddress (String adresse) {
+        this.adresse = adresse;
+    }
+    public String getName () {
+        return this.nom;
+    }
+    public String getSubname () {
+        return this.prenom;
+    }
+    public String getAddress () {
+        return this.adresse;
     }
 }
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -3,8 +3,12 @@ import entities.Personne;
 
 public class TestPersonne {
     public static void main (String[] args) {
-        Personne alan = new Personne("Jean", "Michel", "123 rue bidons");
-        Personne dédé = new Personne("Lou", "Boutin", "45 baker street");
-    }
+        Personne pers1 = new Personne("Jean", "Michel", "45 baker street");
+        Personne pers2 = new Personne("Lou", "Boutin", "45 baker street");
 
+        pers1.personnee("alan", "alan", "alan rue alan");
+        System.out.println(pers1.printName("ballanger", "alan"));
+        pers1.modifyName("la babouche");
+        pers1.modifySubname("alouch");
+    }
 }
